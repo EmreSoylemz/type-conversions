11a69973033de64add7920a051999f447da47060
Emre SÖYLEMEZ <48524826+EmreSoylemz@users.noreply.github.com>
2022-04-19 22:36:31 +0300

    Initial commit for type conversion examples

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace type_conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit Conversions
+            
+            
+            //Explicit Conversions
+        }
+    }
+}
