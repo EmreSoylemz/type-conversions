f7ca16c96c7b545e9650291ea7374a98515c197d
Emre SÖYLEMEZ <48524826+EmreSoylemz@users.noreply.github.com>
2022-04-19 23:52:05 +0300

    code update on type conversions

--- Program.cs
+++ Program.cs
@@ -7,7 +7,19 @@ namespace type_conversions
         static void Main(string[] args)
         {
             // Implicit Conversions
+            byte a  = 5;
+            sbyte b = 30;
+            short c= 10;
+
+            int d = a + b + c;
+
+            Console.WriteLine("d:" + d);
+
+            long h = d;
+            Console.WriteLine("h:" + h);
+
             
+
             
             //Explicit Conversions
         }
